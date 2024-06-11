3b3a16b38410b68fa4c12384edf2580a4482f961
mkheck13 <mkheck13@gmail.com>
2024-06-11 10:05:38 -0700

    convertnumtostringkata

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+// We need a function that can transform a number (integer) into a string.
+
+
+function numberToString(num) {
+    // Return a string of the number here!
+  }
+
+// I remembered that there was method to convert a number to a string
+// So I just returned num with the .toString method
+  function numberToString(num) {
+    return num.toString();
+  }
